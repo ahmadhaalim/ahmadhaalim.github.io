d731eac455257fab9c3ef5d22d3cdc87f064513d
ahmadhaalim <halimprabowo@gmail.com>
2020-11-30 17:06:37 +0700

    using react js component for project tiles

---
+++ script.js
@@ -0,0 +1,23 @@
+function ProjectTile (props){
+    return(
+      <a className="project-tile hvr-grow" href={props.link} target='_blank'>
+        <div className="project-image">
+          <img src={props.img} alt={props.title} />
+        </div>
+        <span className="textlink" href={props.link} target='_blank'>{props.title}</span>
+      </a>
+    )
+}
+
+const tiles = (
+    <div id='projects'>
+      <ProjectTile link='https://github.com/ahmadhaalim/NaiveBayesDiscretization' img='resources/diskritisasi.png' title ='Discretization Effects on Naive Bayes Classification'></ProjectTile>       
+      <ProjectTile link='https://codepen.io/ahmadhaalim/pen/XWromyW' img='resources/Tribute-Page.png' title ='Tribute Page'></ProjectTile>       
+      <ProjectTile link='https://codepen.io/ahmadhaalim/pen/XWWRNBE' img='resources/landingpage.png'title ='Landing Page'></ProjectTile>  
+      <ProjectTile link='https://codepen.io/ahmadhaalim/pen/yLBwjBx' img='resources/Survey-Form.png' title ='Survey Form'></ProjectTile>
+      <ProjectTile link='https://github.com/ahmadhaalim/DanaPulsa_AndroidAutomation' img='resources/gears.png' title ='Android Test Automation'></ProjectTile>             
+    </div>     
+);
+
+ReactDOM.render(tiles, document.querySelector('#pcontainer'));
+
